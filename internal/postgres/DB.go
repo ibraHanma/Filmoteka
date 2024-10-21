5df6329e8950914b52faaf3cda6964e7d2076896
Ибрагим <148527945+ibraHanma@users.noreply.github.com>
2024-10-21 12:59:38 +0300

    Update DB.go

--- internal/postgres/DB.go
+++ internal/postgres/DB.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Connect() (*sql.DB, error) {
-	connStr := "user=postgres password=password dbname=cinematograph host=localhost port=5432 sslmode=disable" // исправлено
+	connStr := "user=postgres password=password dbname=cinematograph host=localhost port=5432 sslmode=disable" 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalln(err)
